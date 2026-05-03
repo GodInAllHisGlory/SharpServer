ede40ae3c3e58d4d21a770730d2e2a72f9234086
Nathan <nathanjohnhaight@gmail.com>
2026-05-03 23:20:11 +0000

    Server now parses HTTP requests

--- SharpServer.cs
+++ SharpServer.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Sharpserver
+{
+    static void Main(string[] args)
+    {
+        const int PORT = 8000;
+        Uri uri = new Uri("http://127.0.0.1");
+        Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        
+        // Bind the socket to the local endpoint
+        socket.Bind(new IPEndPoint(IPAddress.Parse(uri.Host), PORT));
+        
+        // Listen for incoming connections
+        socket.Listen(10);
+        Console.WriteLine("Server listening on " + uri.Host + ":" + PORT);
+        
+        // Accept a connection
+        byte[] buffer = new byte[256];
+        Socket clientSocket = socket.Accept();
+        int bytesRecived = clientSocket.Receive(buffer);
+
+        //Fufill request
+        Request httpRequest = Decoder(buffer, bytesRecived);
+        Console.WriteLine("Verb: " + httpRequest.GetVerb() + " Path: " + httpRequest.GetPath() + " Version: " + httpRequest.GetVersion());
+        Console.WriteLine("Accept-Encoding: " + httpRequest.GetHeader("Accept-Encoding"));
+        Console.WriteLine("Client connected: " + clientSocket.RemoteEndPoint);
+        
+        // Close sockets
+        clientSocket.Close();
+        socket.Close();
+    }
+
+    //Turns the request from the client into a request object 
+    public static Request Decoder(byte[] buffer, int bytesRecived){
+        char[] responseChars = new char[256];
+
+        Encoding.ASCII.GetChars(buffer, 0, bytesRecived, responseChars, 0);
+
+        List<string> request = new string(responseChars).Split("\n").ToList<string>();
+        string[] requestHead = request[0].Trim().Split(" ");
+        request.RemoveAt(0);
+
+        return new Request(requestHead[0], requestHead[1], requestHead[2], request);
+    }
+}
--- request.cs
+++ request.cs
@@ -1,36 +1,50 @@
-using System.ComponentModel.Design;
+using System.Collections.Generic;
 
+//Holds all the information of a http request 
 class Request
 {
     private string verb, path, version;
     private Dictionary<string, string> headers = new Dictionary<string, string>();
-
+    
     public Request(string verb, string path, string version, List<string> headers)
     {
         this.verb = verb;
         this.path = path;
         this.version = version;
 
-        foreach(string header in headers)
+        foreach (string header in headers)
         {
-            string[] data = header.Split(":");
-            this.headers.Add(data[0], data[1]);
+            string[] data = header.Split(":",2);
+            this.headers.Add(data[0].Trim(), data[1].Trim());
         }
     }
 
-    public string getVerb()
+    //Returns http resqest verb
+    public string GetVerb()
     {
-        return this.verb;
+        return verb;
     }
 
-    public string getPath()
+    //Returns the requested path
+    public string GetPath()
     {
-        return this.path;
+        return path;
     }
 
-    public string getVersion()
+    //Returns the version of the http protorcol used
+    public string GetVersion()
     {
-        return this.version;
+        return version;
+    }
+
+    //Returns a specific header vaule
+    public string? GetHeader(string key)
+    {
+        if (headers.TryGetValue(key, out string? header))
+        {
+            return header;
+        }
+        return null;
     }
 
 }
